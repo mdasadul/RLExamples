8eb32f41ecdb378d26c8ec9019871ecfdfef2d50
mdasadul <mdasadul@ualberta.ca>
2018-06-06 09:42:43 -0400

    adding init for DoubleDQN

--- DoubleDQN/brain.py
+++ DoubleDQN/brain.py
@@ -9,6 +9,31 @@ class DoubleDQN(object):
         learning_rate = 0.01,
         gamma = 0.9,
         max_epsilon = 0.9,
-        
+        replace_target_step = 300,
+        double_dqn = False,
+        sess = None,
     ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.max_epsilon = max_epsilon
+        self.replace_target_step = replace_target_step
+        self.double_dqn = double_dqn
+        self.sess = sess 
+
+        if self.sess ==None:
+            self.sess = tf.Session()
+            self.sess.run(tf.global_variables_initializer())
+        
+        self.q = None
+    
+
+    def create_network(self):
+        pass
+    
+    def choose_action(self, observation):
+        pass
+    
+    def learn(self):
         pass
