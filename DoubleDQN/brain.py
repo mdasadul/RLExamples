619b3b511c61c66db175939a2975ccc96e69f2d6
mdasadul <mdasadul@ualberta.ca>
2018-06-06 09:30:43 -0400

    adding doubleDQN

---
+++ DoubleDQN/brain.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import tensorflow as tf 
+
+class DoubleDQN(object):
+    def __init__(
+        self,
+        n_actions,
+        n_features,
+        learning_rate = 0.01,
+        gamma = 0.9,
+        max_epsilon = 0.9,
+        
+    ):
+        pass
