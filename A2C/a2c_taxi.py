e17666145f4ca604dbdc5d5bdfd3b7b335074e6a
mdasadul <mdasadul@ualberta.ca>
2018-06-12 14:37:30 -0400

    adding taxi with actor critic

--- A2C/a2c_taxi.py
+++ A2C/a2c_taxi.py
@@ -34,14 +34,14 @@ class Actor(object):
             self.train_op = tf.train.AdamOptimizer(learning_rate).minimize(-self.exp_v)
 
     def choose_action(self, state):
-        state = state[np.newaxis,:]
+        #state = state[np.newaxis,:]
         ac_prob = self.sess.run(self.ac_prob, feed_dict = {self.state:state})
-        return np.random.choice(np.arange(ac_prob.shape[1]), p=ac_prob.ravel())
+        return np.argmax(ac_prob,1)#np.random.choice(np.arange(ac_prob.shape[1]), p=ac_prob.ravel())
     
 
 
     def learn(self, state, action,error):
-        state = state[np.newaxis,:]
+        #state = state[np.newaxis,:]
         feeddict = {self.state: state, self.action:action,self.td_error:error}
 
         exp_v,_ = self.sess.run([self.exp_v,self.train_op], feed_dict = feeddict)
@@ -75,7 +75,7 @@ class Critic(object):
                 self.train_op = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
             
     def learn(self, state, reward,state_):
-        state,state_ = state[np.newaxis,:],state_[np.newaxis,:]
+        #state,state_ = state[np.newaxis,:],state_[np.newaxis,:]
 
         exp_v = self.sess.run(self.v, feed_dict={self.state:state_})
 
@@ -86,10 +86,9 @@ class Critic(object):
 
 
 if __name__ =='__main__':
-    env = gym.make('CartPole-v0')
-    n_feature = env.observation_space.shape[0]
+    env = gym.make('Taxi-v2')
+    n_feature = env.observation_space.n
     n_action = env.action_space.n
-    print(n_feature)
     sess = tf.Session()
     
     actor = Actor(n_feature, n_action,sess,learning_rate= 0.001)
@@ -97,23 +96,22 @@ if __name__ =='__main__':
     
     sess.run(tf.global_variables_initializer())
 
-    num_tests = 5000
+    num_tests = 1
 
     reward_list =[]
     jlist = []
     for t in range(num_tests):
         state = env.reset()
-        #print(state)
-        print(t)
+        
         rewards = 0
         while True:
-            if t>100:
-                env.render()
-            action = actor.choose_action(state)
+            #if t>100:
+            env.render()
+            action = actor.choose_action(np.eye(n_feature)[state:state+1])
             state_, reward, done, _ = env.step(action)
-            if done: reward = -20
-            td_error = critic.learn(state,reward,state_)
-            actor.learn(state, action, td_error)
+         
+            td_error = critic.learn(np.eye(n_feature)[state:state+1],reward,np.eye(n_feature)[state_:state_+1])
+            actor.learn(np.eye(n_feature)[state:state+1], action, td_error)
             state = state_
 
             rewards += reward
